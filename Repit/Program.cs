3c8f0039a59d8193a87537600a649c1bcb843078
Sara894 <t48250klo@gmail.com>
2022-03-08 15:39:49 +0300

    можно определять переменные для out-параметров при вызове метода

--- Repit/Program.cs
+++ Repit/Program.cs
@@ -28,9 +28,7 @@ namespace Repit
             int sumResult;
             Sum(900, n, out sumResult);
             Console.WriteLine($"Sum: {sumResult}");
-            int area;
-            int perimetr;
-            GetRectangleData(20, 30, out area, out perimetr);
+            GetRectangleData(20, 30, out int area, out int perimetr);
             Console.WriteLine($"area: {area}");
             Console.WriteLine($"perimetr:{perimetr}");
         }
