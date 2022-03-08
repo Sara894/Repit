189fc84ebe88ff826edacf81ce4ac81248f075e0
Sara894 <t48250klo@gmail.com>
2022-03-08 19:38:34 +0300

    доступ через точку

--- Repit/Program.cs
+++ Repit/Program.cs
@@ -2,12 +2,27 @@
 
 namespace Repit
 {
+    class Person
+    {
+        public string name = "Undefined";
+        public int age;
+
+        public void Print()
+        {
+            Console.WriteLine($"Имя {name}, возраст : {age}");
+        }
+    };
     class Program
     {
        
         static void Main(string[] args)
         {
-          
+            Person Gena = new Person();
+            string name = Gena.name;
+            Console.WriteLine(Gena.name);
+            Gena.name = "Геннадий";
+            Gena.age = 14;
+            Console.WriteLine(Gena.age + Gena.name);
         }
     }
 }
