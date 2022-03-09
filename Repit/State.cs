f8b0e663f2cf8e9373cbd6bf66c3dc844075fdbb
Sara894 <t48250klo@gmail.com>
2022-03-09 10:29:00 +0300

    модификаторы доступа

---
+++ Repit/State.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repit
+{
+    class State
+    {
+        //все равно что private string defaultVar
+        string defaultVar = "Undefined";
+        //поле доступно только из текущего класса
+        private string privateVar = "privateVar";
+        //доступно из данного класса или из производных классов которые 
+        //определены в этом же проекте
+        protected private string protectedPrivateVar = "ProtectedPrivateVar";
+        //достпуно из текущего класса и производных
+        protected string protectedVar = "protectedVar";
+        //доступно в любом месте текущего проекта
+        internal string internalVar = "internalVar";
+        //доступно в любом месте текущего проекта и из классов производных в других проектах
+        protected internal string protectedInternalVar = "protectedInternalVar";
+        //достпуно в любом месте текущего проекта и в других проектах тоже
+        public string publicVar = "publicVar";
+
+    }
+}
