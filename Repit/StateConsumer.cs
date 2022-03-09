c18a6626bafea1d2f00bfdfecb6034e2cdfbbd02
Sara894 <t48250klo@gmail.com>
2022-03-09 11:06:15 +0300

    пример работы классов

--- Repit/StateConsumer.cs
+++ Repit/StateConsumer.cs
@@ -6,5 +6,18 @@ namespace Repit
 {
     class StateConsumer
     {
+        public void PrintState()
+        {
+            State state = new State();
+            // переменная internalVar с модификатором internal доступна из любого места текущего проекта
+            // поэтому спокойно присваиваем ей значение
+            Console.WriteLine(state.internalVar);
+
+            // переменная protectedInternalVar так же доступна из любого места текущего проекта
+            Console.WriteLine(state.protectedInternalVar);
+
+            // переменная publicVar общедоступна
+            Console.WriteLine(state.publicVar);
+        }   
     }
 }
